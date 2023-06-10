25aaf6f5b21e1b4d12b3ae78882cf6caab920f04
Yogesh More <yogesh.more2098@gmail.com>
2023-06-10 22:16:23 +0530

    Added: code for displaying contacts from addressbook.

--- src/main/java/org/bridgelabz/AddressBook.java
+++ src/main/java/org/bridgelabz/AddressBook.java
@@ -1,38 +1,50 @@
 package org.bridgelabz;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class AddressBook {
-    public void addContact() {
+    public static ArrayList<Contact> addressBook = new ArrayList<Contact>();
 
+    public Contact inputDetails() {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print(" Please enter the first name: ");
+        System.out.print(" Enter the first name: ");
         String firstName = scanner.next();
 
-        System.out.print(" Please enter the last name: ");
+        System.out.print(" Enter the last name: ");
         String lastName = scanner.next();
 
-        System.out.print(" Please enter the address: ");
+        System.out.print(" Enter the address: ");
         String address = scanner.next();
 
-        System.out.print(" Please enter the city: ");
+        System.out.print(" Enter the city: ");
         String city = scanner.next();
 
-        System.out.print(" Please enter the state: ");
+        System.out.print(" Enter the state: ");
         String state = scanner.next();
 
-        System.out.print(" Please enter the zip: ");
+        System.out.print(" Enter the zip: ");
         int zip = scanner.nextInt();
 
-        System.out.print(" Please enter the phone number: ");
+        System.out.print(" Enter the phone number: ");
         long phone = scanner.nextLong();
 
-        System.out.print(" Please enter the email: ");
+        System.out.print(" Enter the email: ");
         String email = scanner.next();
 
         // Creating a new object of the contact class with the parameter values filled by user
         Contact contact = new Contact(firstName, lastName, address, city, state, zip, phone, email);
+        return contact;
+    }
 
+    public void addContact() {     // Method to create an object of Contact class with user input
+        addressBook.add(inputDetails());
+    }
+
+    public void displayContacts() {
+        for (Contact contact : addressBook) {
+            System.out.println(contact);
+        }
     }
 }
